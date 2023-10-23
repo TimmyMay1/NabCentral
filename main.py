6156a91d879f0c8316a7787faadeceda7018752c
TimmyMay1 <146993294+TimmyMay1@users.noreply.github.com>
2023-10-23 14:59:04 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from tkinter import *
+from tkinter.ttk import *
+style = ttk.Style()
+style.configure("BW.TLabel", foreground="red", background="black")
+l1 = ttk.Label(text="Test", style="BW.TLabel")
+l2 = ttk.Label(text="Test", style="BW.TLabel")
